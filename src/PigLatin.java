0ac095b517b1a396af46f7f6f57e848d02ad3baf
Administrator <Administrator@n-9c4c592af9fe4>
2015-10-31 16:36:00 -0400

    some progres..

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -22,8 +22,7 @@ public class PigLatin {
         System.out.println("Please enter a word");
         String word = in.nextLine();
    
-                    
-
+        
         
         if (word.startsWith("a")
                 || word.startsWith("e")
@@ -31,22 +30,40 @@ public class PigLatin {
                 || word.startsWith("o")
                 || word.startsWith("u")) {
 
+             String firstLetter = word.substring(0,1);
+             String i = word.replace(firstLetter, "i");
              
-
-            pigLatin = word + "hee";
+            String pigLatin = i + "hee";
             System.out.println("In pigLatin: " + pigLatin);
             
         }
         
         
-        if(word.charAt(0) != 'i' && word.charAt(0) !='a'&&word.charAt(0) != 'e'&&word.charAt(0) != 'u'&&word.charAt(0) != 'o' ){
             
+          if(word.charAt(2) == 'i' && word.charAt(2) =='a'&&word.charAt(2) == 'e'&&word.charAt(2) == 'u'&&word.charAt(2) == 'o' ){
+              String first = word.substring(0,2);
+            String last = word.substring(2);
+            String pigLatin = last + first + "ee"; 
+            System.out.println("IN Piglatin: " + pigLatin);
+          
+          }else{
+            // This is for words that have a vowel after the first letter
             String first = word.substring(0,1);
             String last = word.substring(1);
-            
-              pigLatin = last + first + "ee"; 
+            String firstLetter = word.substring(0,2);
+            String pigLatin = word.replace(firstLetter, "i");
+             pigLatin = pigLatin + first + "ee"; 
+             
             
             System.out.println("Piglatin: " + pigLatin);}
+            
+        
+            
+        
+            
+            
+                
+           
     
     }
 }
