55a87d61295bbcc175047fe3cb5790f19aa1cf2a
bettk6516 <bettk6516@HRH-IC0027345.wrdsb.ca>
2015-11-06 13:24:19 -0500

    Assigment 5

--- src/Assigment5.java
+++ src/Assigment5.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author bettk6516
@@ -18,34 +17,95 @@ public class Assigment5 {
     public static void main(String[] args) {
         //improts Scanner 
         Scanner input = new Scanner(System.in);
-       //text out and user input of the desired word to be translated
-        System.out.println("Enter the word you wish to translate(type END to quit the program)");
-        String word = input.nextLine();
+
         //loops forever 
-        while(true){
-       //if the word entered is END the program ends
-            if(word.equals("END") ){
-        break;
-    }
-        
+        while (true) {
+            //if the word entered is END the program ends
+            //text out and user input of the desired word to be translated
+            System.out.println("Enter the word you wish to translate in lower case(type END to quit the program)");
+
+            String word = input.nextLine();
+            if (word.equals("END")) {
+                break;
+            }
+
             String pLat = word;
             //if the word starts with a vowel
-         if(pLat.startsWith("a") 
-                 || pLat.startsWith("e") 
-                 || pLat.startsWith("i") 
-                 || pLat.startsWith("o")
-                 || pLat.startsWith("u")){
-           
-            
-        String firstV = pLat.substring(0);
-        String restOfWord = pLat.substring(1);
-             System.out.println(firstV);
+            if (pLat.startsWith("a")
+                    || pLat.startsWith("e")
+                    || pLat.startsWith("i")
+                    || pLat.startsWith("o")
+                    || pLat.startsWith("u")
+                    || pLat.startsWith("A")
+                    || pLat.startsWith("E")
+                    || pLat.startsWith("I")
+                    || pLat.startsWith("O")
+                    || pLat.startsWith("U")) {
+
+
+
+
+
+                //takes the rest of word with the exepltion of the first letter
+                String restOfWord = pLat.substring(1);
+                System.out.println("i" + restOfWord + "hee");
+
+                System.out.println("");
+
+
+            }
+            //if the word doesnt start with a voewl 
+            if (!pLat.startsWith("a")
+                    || !pLat.startsWith("e")
+                    || !pLat.startsWith("i")
+                    || !pLat.startsWith("o")
+                    || !pLat.startsWith("u")) {
+
+                //grabs tthe location of each letter 
+                int findA = pLat.indexOf("a");
+                int findE = pLat.indexOf("e");
+                int findI = pLat.indexOf("i");
+                int findO = pLat.indexOf("o");
+                int findU = pLat.indexOf("u");
+
+
+
+                //if the voewl is not in the word, the value of that letter is set to a ungodly number
+                if (findA == -1) {
+                    findA = 100000000;
+                }
+                if (findE == -1) {
+                    findE = 100000000;
+                }
+                if (findI == -1) {
+                    findI = 100000000;
+                }
+                if (findU == -1) {
+                    findU = 100000000;
+                }
+
+
+                //if a is in the word,and occurs before the other voewls the word is choped there and the translate rules applyed 
+                if (findA != 100000000) {
+                    if ((findA < findE)
+                            || findA < findI
+                            || findA < findO
+                            || findA < findU) {
+                    String firstA = pLat.substring(0,findA);
+                    String lastA = pLat.substring(findA);
+                    
              
-         
-        
-             
-         }
-         
-        }           
+                    pLat = "i" + lastA + firstA;
+                       
+                     System.out.println(pLat);
+                    }
+
+
+                }
+
+
+
+            }
+        }
     }
 }
