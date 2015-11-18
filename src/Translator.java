f713485377de16bdc30cd6f148a9600fea2b07d3
malcr1272 <malcr1272@HRH-IC0027320.wrdsb.ca>
2015-11-18 14:06:03 -0500

    assignment 5

--- src/Translator.java
+++ src/Translator.java
@@ -3,7 +3,6 @@
  * and open the template in the editor.
  */
 
-
 import java.util.Scanner;
 
 /**
@@ -17,42 +16,56 @@ public class Translator {
      */
     public static void main(String[] args) {
         //make scanner
-       Scanner in = new Scanner(System.in);
-       
-       //create string for the word
-       String word = "";
-       
-       //make while loop to ask for words to translate
-       while (!word.equals("END")){
-       System.out.println("Please enter a word to translate(Type END to quit the program): ");
-       word = in.nextLine();
-           
-       if(word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u")){
-       String latin  = word.replace(""+word.charAt(0), "i");
-       
-       if (word.endsWith("a") || word.endsWith("e") || word.endsWith("i") || word.endsWith("o") || word.endsWith("u")){
-           latin = latin + "hee";
-       }else{
-           latin = latin + "ee";
-       }
-       System.out.println(word + " in Mattenenglisch is " + latin);
-       
-       }else{
-       int length = word.length();
-       for(int i = 0; i < length; i = i+ 1){
-           if(word.charAt(i) == 'a' || 
-              word.charAt(i) == 'e' ||
-              word.charAt(i) == 'i' ||
-              word.charAt(i) == 'o' ||
-              word.charAt(i) == 'u'){
-                      
-             String vowel = word.substring(0, i);
-               
-           }
-       }
-           
-       }
-           
-       }
+        Scanner in = new Scanner(System.in);
+
+        //create string for the word
+        String word = "";
+
+        //make while loop to ask for words to translate
+        while (!word.equals("END")) {
+            System.out.println("Please enter a word to translate(Type END to quit the program): ");
+            word = in.nextLine();
+
+            if (word.startsWith("a") 
+                    || word.startsWith("e") 
+                    || word.startsWith("i") 
+                    || word.startsWith("o") 
+                    || word.startsWith("u")) {
+                String latinvowel = word.replace("" + word.charAt(0), "i");
+
+                if (word.endsWith("a") 
+                        || word.endsWith("e") 
+                        || word.endsWith("i") 
+                        || word.endsWith("o") 
+                        || word.endsWith("u") 
+                        || word.endsWith("y")) {
+                    latinvowel = latinvowel + "hee";
+                } else {
+                    latinvowel = latinvowel + "ee";
+                }
+                System.out.println(word + " in Mattenenglisch is " + latinvowel);
+
+            } else {
+                int length = word.length();
+                for (int i = 0; i < length; i = i + 1) {
+                    if        ((word.charAt(i) == 'a')
+                            ||(word.charAt(i) == 'e')
+                            ||(word.charAt(i) == 'i')
+                            ||(word.charAt(i) == 'o')
+                            ||(word.charAt(i) == 'u')
+                            ||(word.charAt(i) == 'y')) {
+
+                        String beforevowel = word.substring(0, i);
+                        String aftervowel = word.substring(i+1);
+                       
+
+                        aftervowel = ("i" + aftervowel + beforevowel + "ee");
+
+                        System.out.println(word + " in Mattenenglisch is " + aftervowel);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
