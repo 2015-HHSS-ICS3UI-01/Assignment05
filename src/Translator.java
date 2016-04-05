a4c3c367eb8a2cabb306248d450d81a77df5da1f
pintm1551 <pintm1551@HRH-IC0027344.wrdsb.ca>
2016-04-05 08:16:03 -0400

    Assignment05

---
+++ src/Translator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pintm1551
+ */
+public class Translator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    
+    }
+}
