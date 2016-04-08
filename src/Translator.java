adc981f6928b8a9f09f2b58206fa88a59a964018
pintm1551 <pintm1551@HRH-IC0027344.wrdsb.ca>
2016-04-08 08:53:18 -0400

    Translator

--- src/Translator.java
+++ src/Translator.java
@@ -1,8 +1,10 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author pintm1551
@@ -13,8 +15,25 @@ public class Translator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-    
+        // TODO code application logic here                             
+        Scanner input = new Scanner(System.in);
+        String eng;
+        String wrd = "ee";
+        int x = 0;
+        System.out.println("Please enter a word to translate(Type end to quit the program):");
+        eng = input.nextLine();
+        eng = eng.toLowerCase();
+        while (eng != "end") {
+            String pig = eng;
+            while (!pig.endsWith("ee")) {
+                while (!pig.startsWith("a") && !pig.startsWith("e") && !pig.startsWith("i") && !pig.startsWith("o") && !pig.startsWith("u") && !pig.startsWith("y")) {
+                    wrd = pig.substring(0,1) + wrd;
+                }
+                pig = pig + "ee";
+            }
+            System.out.println(eng + " in Mattenenglisch is " + pig);
+            System.out.println("Please enter a word to translate(Type END to quit the program):");
+            eng = input.nextLine();
+        }
     }
 }
