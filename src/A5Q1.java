7a98e1f20217496da5669c878a13b61be72e609c
denis <denis@192.168.0.22>
2016-04-14 07:17:08 -0700

    Assignment05 (Initial commit)

--- src/A5Q1.java
+++ src/A5Q1.java
@@ -19,41 +19,23 @@ public class A5Q1 {
         Scanner input = new Scanner(System.in);
 
         //Ask user to input the word wished to be translated
-        System.out.println("Please enter a word to translate.");
+        System.out.println("Please enter a word to translate in matten English.");
         String word = input.nextLine();
         //Saving the vowels
-        int a = word.indexOf("a");
-        int e = word.indexOf("e");
-        int i = word.indexOf("i");
-        int o = word.indexOf("o");
-        int u = word.indexOf("u");
-        int y = word.indexOf("y");
-
-        // Change all 'a' to '@'
-        String newWord = word.replace("a", "ee");
-        System.out.println(newWord);
-
-        //using StringBuilder
-        StringBuilder sBuilder = new StringBuilder(word);
-        // Find the first occurring 'a'
-        int firstVowel = sBuilder.indexOf("a");
-        // If an 'a' is found
-        if (firstVowel != -1) {
-            // Remove the 'a'
-            sBuilder.deleteCharAt(firstVowel);
-            // put an @ symbol in its place
-            sBuilder.insert(firstVowel, "ee");
-            sBuilder.append(word);
+        if (word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u") || word.startsWith("y")) {
+            word = word.replaceFirst("a", "i");
+            if (word.startsWith("e")) {
+                word = word.replaceFirst("e", "i");
+            } else {
+                word = word.replace("e", "i");
+            }
+            word = word.replace("i", "i");
+            word = word.replace("o", "i");
+            word = word.replace("u", "i");
+            word = word.replace("y", "i");
         }
-        int firstL = sBuilder.indexOf("a");
-        // Found an L
-        if (firstL != -1) {
-            // Grabs all the letters up until the first L
-            String starting = sBuilder.substring(0, firstL);
-            // Gets rid of everything up until the L
-            sBuilder.delete(0, firstL);
-            // adds the starting to the end
-            sBuilder.append(starting);
-        }
+        System.out.println("Your word translated is " + word);
+
+        //if(word.contains("a") || word.contains("e") || word.contains("a") || word.contains("a") || word.contains("a") || word.contains("a"))
     }
 }
