13ee7a0634bb3cc154997a1264f43abd0e9baa74
pintm1551 <pintm1551@HRH-IC0027344.wrdsb.ca>
2016-04-14 08:12:56 -0400

    Translator

--- src/Translator.java
+++ src/Translator.java
@@ -16,24 +16,36 @@ public class Translator {
      */
     public static void main(String[] args) {
         // TODO code application logic here                             
-        Scanner input = new Scanner(System.in);
-        String eng;
-        String wrd = "ee";
-        int x = 0;
-        System.out.println("Please enter a word to translate(Type end to quit the program):");
-        eng = input.nextLine();
-        eng = eng.toLowerCase();
+        Scanner input = new Scanner(System.in);                         //scanner
+        String eng = ".";                                               //Initializes string eng
+        int x = 0;                                                      //for counting length                                                 //for ending game
+        int pos = 0;                                                    //for counting letters removed
         while (eng != "end") {
-            String pig = eng;
-            while (!pig.endsWith("ee")) {
-                while (!pig.startsWith("a") && !pig.startsWith("e") && !pig.startsWith("i") && !pig.startsWith("o") && !pig.startsWith("u") && !pig.startsWith("y")) {
-                    wrd = pig.substring(0,1);
-                }
-                pig = pig + "ee";
+            System.out.println("Please enter a word to translate(Type end to quit the program):");  //opening statement
+            eng = input.nextLine();                                     //finds eng
+            eng = eng.toLowerCase();                                    //converts to lower case
+            if (eng.equals("end")) {                                          
+                break;
             }
-            System.out.println(eng + " in Mattenenglisch is " + pig);
-            System.out.println("Please enter a word to translate(Type END to quit the program):");
-            eng = input.nextLine();
+            String pig = eng;                                           //sets pig as eng to start translating
+            while (!pig.startsWith("a") && !pig.startsWith("e") && !pig.startsWith("i") && !pig.startsWith("o") && !pig.startsWith("u")) {  //while a vowel is first
+                pos = pos + 1;                                         //counts letters removed
+                pig = pig + pig.substring(0, 1);                       //adds the first letter of pig to the back
+                x = pig.length();                                       //counts the length of pig
+                pig = pig.substring(1, x);                              //removes the first letter of pig
+                if (pig.startsWith("y") && pos == 2 || x - 1 == 2) {
+                    break;
+                }
+            }
+            x = pig.length();                                           //checks length
+            pig = pig.substring(1, x);                                  //deletes first letter
+            pig = "i" + pig;                                            //replaces first letter with i
+            if (!pig.endsWith("h") && pos == 0) {                       //if the word starts with a vowel and ends in h
+                pig = pig + "hee";                                          //adds hee
+            } else {
+                pig = pig + "ee";                                           //adds ee
+            }
+            System.out.println(eng + " in Mattenenglisch is " + pig);   //final statement
         }
     }
 }
